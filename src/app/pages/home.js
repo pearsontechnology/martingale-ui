7e56dacca526ac71eca7be8494504b6828c32c2b
jdarling <jeremy.darling@gmail.com>
2017-05-11 19:09:53 -0500

    Kube pages

--- src/app/pages/home.js
+++ src/app/pages/home.js
@@ -11,88 +11,122 @@ const Page = (props)=>pageSchemaToReact({
   layout: {
     $type: 'div',
     children: {
-      $type: 'HeaderPage',
-      props: {
-        title: 'Kong Overview'
-      },
-      children: {
-        $type: 'Row',
-        children: [
-          {
-            $type: 'Panel',
+      $type: 'Page',
+      children: [
+        {
+          $type: 'PageHeader',
+          props: {
+            title: 'Kubernetes Namespaces'
+          }
+        },
+        {
+          $type: 'Panel',
+          children: {
+            $type: 'Provider',
             props: {
-              title: 'Connections Status',
-              inset: true,
-              md: 6,
-              sm: 12
-            },
-            children: {
-              $type: 'Provider',
+              provide: {
+                data: {
+                  url: `${KUBE_ROOT}/v1/namespaces`,
+                  root: 'items',
+                  mapper: {$mapper: 'props.map(r=>r.metadata).map(m=>{return {name: m.name, created: m.creationTimestamp}})'}
+                }
+              },
+              Component: {$component: 'ActionTable'},
               props: {
-                provide: {
-                  data: {
-                    url: `${KONG_ROOT}/status`,
-                    mapper:{$mapper: 'props.server?[props.server]:null'},
-                  }
-                },
-                Component: {$component: 'BarChart'},
-                props: {
-                  bars:['connections_active', 'connections_waiting',
-                  'connections_reading', 'connections_writing']
+                actions: {
+                  link: `/namespace/\${name}/pods`,
+                  caption: 'View Pods',
+                  btnStyle: 'primary'
                 }
               }
             }
-          },
-          {
-            $type: 'Panel',
-            props: {
-              title: 'Connections Overview',
-              inset: true,
-              md: 3,
-              sm: 12
-            },
-            children: {
-              $type: 'Provider',
+          }
+        },
+        {
+          $type: 'PageHeader',
+          props: {
+            title: 'Kong Overview'
+          }
+        },
+        {
+          $type: 'Row',
+          children: [
+            {
+              $type: 'Panel',
               props: {
-                provide: {
-                  data: {
-                    url: `${KONG_ROOT}/status`,
-                    mapper:{$mapper: 'props.server?[props.server]:null'},
-                  }
-                },
-                Component: {$component: 'BarChart'},
+                title: 'Connections Status',
+                inset: true,
+                md: 6,
+                sm: 12
+              },
+              children: {
+                $type: 'Provider',
                 props: {
-                  bars:['connections_handled', 'connections_accepted']
+                  provide: {
+                    data: {
+                      url: `${KONG_ROOT}/status`,
+                      mapper:{$mapper: 'props.server?[props.server]:null'},
+                    }
+                  },
+                  Component: {$component: 'BarChart'},
+                  props: {
+                    bars:['connections_active', 'connections_waiting',
+                    'connections_reading', 'connections_writing']
+                  }
                 }
               }
-            }
-          },
-          {
-            $type: 'Panel',
-            props: {
-              title: 'Timers',
-              inset: true,
-              md: 3,
-              sm: 12
             },
-            children: {
-              $type: 'Provider',
+            {
+              $type: 'Panel',
               props: {
-                provide: {
-                  data: {
-                    url: `${KONG_ROOT}`,
-                    mapper:{$mapper: 'props.timers?[props.timers]:null'},
-                  }
-                },
-                Component: {$component: 'BarChart'},
+                title: 'Connections Overview',
+                inset: true,
+                md: 3,
+                sm: 12
+              },
+              children: {
+                $type: 'Provider',
                 props: {
-                  bars:['running', 'pending']
+                  provide: {
+                    data: {
+                      url: `${KONG_ROOT}/status`,
+                      mapper:{$mapper: 'props.server?[props.server]:null'},
+                    }
+                  },
+                  Component: {$component: 'BarChart'},
+                  props: {
+                    bars:['connections_handled', 'connections_accepted']
+                  }
                 }
               }
-            }
-          },
-        ]
-      }
+            },
+            {
+              $type: 'Panel',
+              props: {
+                title: 'Timers',
+                inset: true,
+                md: 3,
+                sm: 12
+              },
+              children: {
+                $type: 'Provider',
+                props: {
+                  provide: {
+                    data: {
+                      url: `${KONG_ROOT}`,
+                      mapper:{$mapper: 'props.timers?[props.timers]:null'},
+                    }
+                  },
+                  Component: {$component: 'BarChart'},
+                  props: {
+                    bars:['running', 'pending']
+                  }
+                }
+              }
+            },
+          ]
+        }
+      ]
     }
   },
   components: Components,
--- src/app/pages/index.js
+++ src/app/pages/index.js
@@ -1,9 +1,11 @@
 import Home from './home';
 import KitchenSink from './kitchensink';
 import APIs from './apis';
+import Kube from './kube';
 
 export default {
   Home,
   KitchenSink,
-  ...APIs
+  ...APIs,
+  ...Kube
 };

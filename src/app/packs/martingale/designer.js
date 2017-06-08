92e207894fb4407dee39158d5778044648489974
jdarling <jeremy.darling@gmail.com>
2017-06-08 10:45:00 -0500

    Useable designer

--- src/app/packs/martingale/designer.js
+++ src/app/packs/martingale/designer.js
@@ -2,14 +2,18 @@ import React from 'react';
 import {
   pageSchemaToReact
 } from 'martingale-page-schema';
+import pathToRegexp from 'path-to-regexp';
 import Components from '../../../components';
+
 const COMPONENT_NAMES = Object.keys(Components);
 const {
   HeaderPage,
-  PageHeader,
   Panel,
   Row,
-  Col
+  Col,
+  Form,
+  Button,
+  Alert
 } = Components;
 
 const SAMPLE_SOURCE=`{
@@ -22,9 +26,31 @@ const SAMPLE_SOURCE=`{
     props: {
       inset: true
     },
-    children: 'Hello World!'
+    children: {$map: '\`Hello \${params.name||"world"}!\`'}
   }
 }`;
+
+const UI_SCHEMA = {
+  params: {
+    'ui:widget': 'textarea'
+  },
+  layout: {
+    'ui:widget': 'textarea'
+  }
+};
+
+const FORM_SCHEMA = {
+  type: 'object',
+  required: ['path', 'layout'],
+  properties: {
+    path: {type: 'string'},
+    params: {type: 'string'},
+    sideNav: {type: 'boolean', default: false},
+    icon: {type: 'string'},
+    caption: {type: 'string'},
+    layout: {type: 'string'}
+  }
+};
 
 const getComponentDescription = (componentName)=>{
   const component = Components[componentName];
@@ -36,36 +62,33 @@ const getComponentDescription = (componentName)=>{
   ];
 };
 
-class Editor extends React.Component{
-  sourceChanged(e){
-    e.preventDefault();
-    const value = this.editor.value;
-    this.props.onChange && this.props.onChange(value);
-  }
+const components = <dl>{COMPONENT_NAMES.sort().map(getComponentDescription)}</dl>;
 
+class Editor extends React.Component{
   render(){
-    const style={
-      width: '100%',
-      minHeight: '200px'
-    };
     const {
-      source
+      onChange,
+      ...data
     } = this.props;
-    return <textarea ref={(editor)=>this.editor=editor} onChange={this.sourceChanged.bind(this)} style={style} defaultValue={source} />;
+    return (
+      <Row>
+        <Col md={10}>
+          <Panel inset={true}>
+            <Form schema={FORM_SCHEMA} uiSchema={UI_SCHEMA} data={data} onChange={onChange} />
+          </Panel>
+        </Col>
+        <Panel header="Available Components" inset={true} md={2} maxHeight={500}>
+          {components}
+        </Panel>
+      </Row>
+    );
   }
 };
 
-class Designer extends React.Component{
-  constructor(){
+class Preview extends React.Component{
+  constructor(props){
     super();
-    this.state = {
-      source: SAMPLE_SOURCE,
-      params: {}
-    };
-  }
-
-  editorChanged(source){
-    this.setState({source});
+    this.state = {path: props.path};
   }
 
   evalSource(source){
@@ -85,44 +108,105 @@ class Designer extends React.Component{
   }
 
   getLayout(){
-    return this.evalSource(this.state.source);
+    return this.evalSource(this.props.layout);
   }
 
-  parametersChanged(newParams){
-    const params = this.evalSource(newParams);
-    if(params instanceof Error){
-      return;
+  getProps(){
+    const keys = [];
+    const path = this.state.path;
+    const re = pathToRegexp(this.props.path, keys);
+    const args = re.exec(path)||[];
+    let props = {};
+    for(let i = 0; i<keys.length; i++){
+      const key = keys[i].name;
+      const value = args[i+1];
+      props = Object.assign({}, props, {[key]: value});
     }
-    this.setState({params});
+    return props;
+  }
+
+  updateView(e){
+    e.preventDefault();
+    const path = this.editor.value;
+    this.setState({path});
+  }
+
+  getPreview(){
+    const props = this.getProps();
+    const layout = this.getLayout();
+    try{
+      return layout instanceof Error?<div>{layout.toString()}</div>:pageSchemaToReact({
+        layout,
+        components: Components,
+        props
+      });
+    }catch(e){
+      const message = e.toString();
+      const args = e.args;
+      const source = e.source;
+      const data = e.data;
+      return (
+        <Panel inset={true}>
+          <Alert type="danger"><strong>Error:</strong> {message}</Alert>
+          <p><strong>Args:</strong> <pre>{args.join(', ')}</pre></p>
+          <p><strong>Data:</strong> <pre>{JSON.stringify(data, null, 2)}</pre></p>
+          <p><strong>Source:</strong> <pre>{source}</pre></p>
+        </Panel>
+      );
+    }
   }
 
   render(){
-    const layout = this.getLayout();
-    const preview = layout instanceof Error?`${layout.toString()}`:pageSchemaToReact({
-              layout,
-              components: Components,
-              props: this.state.params
-            });
-    const components = <dl>{COMPONENT_NAMES.sort().map(getComponentDescription)}</dl>
+    const preview = this.getPreview();
+
     return (
       <div>
-        <HeaderPage title="Pack Designer">
-          <Row>
-            <Col md={10}>
-              <Panel header="Parameters" inset={true}>
-                <Editor source={JSON.stringify(this.state.params, null, 2)} onChange={this.parametersChanged.bind(this)}/>
-              </Panel>
-              <Panel header="Source" inset={true}>
-                <Editor source={this.state.source} onChange={this.editorChanged.bind(this)}/>
-              </Panel>
-            </Col>
-            <Panel header="Available Components" inset={true} md={2} maxHeight={500}>
-              {components}
-            </Panel>
-          </Row>
-        </HeaderPage>
-        <PageHeader>Preview</PageHeader>
+        <Panel inset={true}>
+          <div><input className="form-control" ref={(editor)=>this.editor=editor} defaultValue={this.state.path} /></div>
+          <Button className="btn-primary" onClick={this.updateView.bind(this)}>Update</Button>
+        </Panel>
         {preview}
+      </div>
+    );
+  }
+};
+
+class Designer extends React.Component{
+  constructor(){
+    super();
+    this.state = {
+      mode: 'edit',
+      path: '/hello/:name',
+      layout: SAMPLE_SOURCE
+    };
+  }
+
+  formUpdated({errors, formData}){
+    if(errors && errors.length){
+      return;
+    }
+    this.setState(formData);
+  }
+
+  switchMode(e){
+    e.preventDefault();
+    this.setState({mode: this.state.mode === 'edit'?'preview':'edit'});
+  }
+
+  render(){
+    const {
+      mode,
+      ...options
+    } = this.state;
+    const editing = mode === 'edit';
+    const actionCaption = editing?'Preview':'Edit';
+    const titlebar = <span>Pack Designer <Button onClick={this.switchMode.bind(this)}>{actionCaption}</Button></span>;
+    const view = editing?<Editor onChange={this.formUpdated.bind(this)} {...options} />:<Preview {...options} />;
+    return (
+      <div>
+        <HeaderPage title={titlebar}>
+          {view}
+        </HeaderPage>
       </div>
     );
   }

4ade09ff38dd2575166b6cd524149a5925f2afca
jdarling <jeremy.darling@gmail.com>
2017-08-10 16:36:04 -0500

    Better layout through DataView and css

--- src/components/pages/dataview/index.js
+++ src/components/pages/dataview/index.js
@@ -5,47 +5,14 @@ import {
 } from 'martingale-provider';
 
 import {
-  Panel,
   PanelFooter,
   PanelTitle,
-  ActionTable,
-  Table,
-  JsonView
+  DataView
 } from 'martingale-ui-components';
-
-import {
-  betterType
-} from 'martingale-utils';
 
 import Page from '../page';
 
 import {PageHeader} from '../header';
-
-const View = ({data, actions, footerContents, inset = true, ...props})=>{
-  if(!data){
-    return (
-        <Panel inset={true}>
-          <span className="loading">Loading...</span>
-        </Panel>
-      );
-  }
-  const dataType = betterType(data);
-  const wrap = (children, {inset = true})=>{
-    return (
-        <Panel inset={inset}>
-          {children}
-          {footerContents}
-        </Panel>
-      );
-  };
-  if(dataType === 'array'){
-    if(actions){
-      return wrap(<ActionTable data={data} actions={actions} {...props} />, {inset: false});
-    }
-    return wrap(<Table data={data} {...props} />, {inset: false});
-  }
-  return wrap(<JsonView json={data} inset={true} {...props} />, {inset});
-};
 
 const getFooterContents = (footer, props, defaultValue)=>{
   if(typeof(footer) === 'function'){
@@ -93,7 +60,7 @@ const DataViewPage=(props)=>{
     <Page>
       <PageHeader>{Icon?<PanelTitle>{React.createElement(Icon)} {title}</PanelTitle>:title}</PageHeader>
       <Provider
-        Component={View}
+        Component={DataView}
         provide={provide}
         props={Object.assign(rest, {footerContents})}
         />

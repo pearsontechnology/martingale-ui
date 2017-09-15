87baa7b7c5d4d59d5ddca92c9e6b06b8ac8d6096
jdarling <jeremy.darling@gmail.com>
2017-09-15 08:55:53 -0500

    New charts

--- src/app/packs/martingale/kitchensink.js
+++ src/app/packs/martingale/kitchensink.js
@@ -101,10 +101,10 @@ const lineData = [
 const lineLines = ['uv', 'pv', 'amt'];
 const lineXAxisKey = 'name';
 const barData = [
-    {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+    {name: 'Page A', uv: -4000, pv: 2400, amt: 2400},
     {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
     {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-    {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+    {name: 'Page D', uv: -2780, pv: 3908, amt: 2000},
     {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
     {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
     {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
@@ -115,6 +115,15 @@ const pieData = [
     {name: 'Group A', value: 400}, {name: 'Group B', value: 300},
     {name: 'Group C', value: 300}, {name: 'Group D', value: 200}
   ];
+const radarData = [
+    { subject: 'Math', A: 120, B: 110, fullMark: 150 },
+    { subject: 'Chinese', A: 98, B: 130, fullMark: 150 },
+    { subject: 'English', A: 86, B: 130, fullMark: 150 },
+    { subject: 'Geography', A: 99, B: 100, fullMark: 150 },
+    { subject: 'Physics', A: 85, B: 90, fullMark: 150 },
+    { subject: 'History', A: 65, B: 85, fullMark: 150 },
+  ];
+
 
 const layout = {
   $type: 'Page',
@@ -502,6 +511,65 @@ const layout = {
               $type: 'ScatterChart',
               props: {
                 scatters: scatterData
+              }
+            }
+          ]
+        },
+        {
+          $type: 'Panel',
+          props: {
+            inset: true,
+            title: 'Composed Chart',
+            sm: 12,
+            lg: 6
+          },
+          children: [
+            {
+              $type: 'ComposedChart',
+              props: {
+                data: lineData,
+                lines: ['amt'],
+                areas: ['uv'],
+                bars: ['pv'],
+                XAxisKey: lineXAxisKey
+              }
+            }
+          ]
+        },
+        {
+          $type: 'Panel',
+          props: {
+            inset: true,
+            title: 'Radar Chart',
+            sm: 12,
+            lg: 6
+          },
+          children: [
+            {
+              $type: 'RadarChart',
+              props: {
+                data: radarData,
+                radars: ['A', 'B'],
+                axisKey: 'subject'
+              }
+            }
+          ]
+        },
+        {
+          $type: 'Panel',
+          props: {
+            inset: true,
+            title: 'Radial Bar Chart',
+            sm: 12,
+            lg: 6
+          },
+          children: [
+            {
+              $type: 'RadialBarChart',
+              props: {
+                data: barData,
+                bars: ['uv'],
+                axisKey: 'subject'
               }
             }
           ]

42717cd2ec0a6869c0f5ed0cd0938ff218facf3a
Jeremy Darling <jeremy.darling@gmail.com>
2018-07-19 09:16:40 -0500

    Updating docs for REACT_APP_CONFIG_FILE_LOCATION

--- src/app/config/config.js
+++ src/app/config/config.js
@@ -1,70 +1,71 @@
 import React from 'react';
-import {
-  fetch
-} from '@martingale/utils';
+import { fetch } from '@martingale/utils';
 import linkSpecs from './specs';
 import linkPacks from './packs';
 import linkSideNav from './sidenav';
 
-import {Error} from '@martingale/ui-components';
+import { Error } from '@martingale/ui-components';
 
-const DEFAULT_CONFIG = {state: 'noconfig'};
+const DEFAULT_CONFIG = { state: 'noconfig' };
 
-class Config extends React.Component{
-  constructor(){
+class Config extends React.Component {
+  constructor() {
     super();
-    this.state = {state: 'loading'};
+    this.state = { state: 'loading' };
   }
 
-  trapError(next, ...args){
-    return (err, data)=>{
-      if(err){
-        return this.setState({state: 'error', error: err});
+  trapError(next, ...args) {
+    return (err, data) => {
+      if (err) {
+        return this.setState({ state: 'error', error: err });
       }
       return next(data, ...args);
     };
   }
 
-  configurationLoaded(config = {}){
-    linkSpecs(config,
-      this.trapError(linkPacks,
-        this.trapError(linkSideNav,
-          this.trapError((config)=>this.setState(Object.assign(config, {state: 'loaded'})))
+  configurationLoaded(config = {}) {
+    linkSpecs(
+      config,
+      this.trapError(
+        linkPacks,
+        this.trapError(
+          linkSideNav,
+          this.trapError((config) =>
+            this.setState(Object.assign(config, { state: 'loaded' }))
+          )
         )
       )
     );
   }
 
-  componentDidMount(){
-    fetch('/config.json')
-      .then((response)=>{
-        if(response.status !== 200){
-          return this.setState({state: 'noconfig'});
+  componentDidMount() {
+    fetch(process.env.REACT_APP_CONFIG_FILE_LOCATION || '/config.json')
+      .then((response) => {
+        if (response.status !== 200) {
+          return this.setState({ state: 'noconfig' });
         }
         return response.json();
       })
-      .then((config)=>{
-        if(config){
+      .then((config) => {
+        if (config) {
           return this.configurationLoaded(config);
         }
         return this.configurationLoaded(DEFAULT_CONFIG);
       });
   }
 
-  renderError(){
-    const {
-      error
-    } = this.state;
+  renderError() {
+    const { error } = this.state;
     return <Error error={error} />;
-  };
+  }
 
-  render(){
+  render() {
     const Child = this.props.child;
-    if(this.state.state === 'error'){
+    if (this.state.state === 'error') {
       return this.renderError();
     }
     return <Child {...this.state} />;
   }
-};
+}
 
 export default Config;
--- src/app/packs/docs/pages/configurationbasics.js
+++ src/app/packs/docs/pages/configurationbasics.js
@@ -1,25 +1,22 @@
-const documentContents = `In order for Martingale to present a User Interface to interact with systems it needs to know what exists and how to interact with those things.  The configuration file provides Martingale with a starting point on what to load and make available to end users.
+const documentContents = `In order for Martingale to present a User Interface to interact with systems it needs to know what exists and how to interact with those things. The configuration file provides Martingale with a starting point on what to load and make available to end users.
 
-When Martingale is loaded by the browser it will make a request for /config.json file.  You can either edit this file directly (it is kept in martingale/ui/public/config.json) or you can proxy the request (recommended) to return your own custom config.json file (more on this in Installing Packs and Kong sections).
+When Martingale is loaded by the browser it will make a request for /config.json (by default) file. You can either edit this file directly (it is kept in martingale/ui/public/config.json), you can proxy the request to return your own custom config.json file (more on this in Installing Packs and Kong sections), or you can set the REACT_APP_CONFIG_FILE_LOCATION environment variable and create a custom build (recommended).
 
 The default configuration file for Martingale looks as follows:
 
-\`\`\` json
+\`\`\`json
 {
-  "packs": [
-    "Martingale",
-    "Documentation"
-  ]
+  "packs": ["Martingale", "Documentation"]
 }
 \`\`\`
 
-This tells Martingale to load the two included packs that it ships with, the first is the "Martingale" pack that contains a homepage, kitchen sink, and a very basic page builder.  The second contains the documentation for Martingale.
+This tells Martingale to load the two included packs that it ships with, the first is the "Martingale" pack that contains a homepage, kitchen sink, and a very basic page builder. The second contains the documentation for Martingale.
 
-Unlike many systems, Martingale Packs don't have to be compiled in, downloaded, installed, or generally reside anywhere within the Martingale instance running.  This means that for the most part you never actually have to "upgrade" Martingale itself unless you want to take advantage of new features or components that are not available in your current version.  More on this in the Installing Packs section.
+Unlike many systems, Martingale Packs don't have to be compiled in, downloaded, installed, or generally reside anywhere within the Martingale instance running. This means that for the most part you never actually have to "upgrade" Martingale itself unless you want to take advantage of new features or components that are not available in your current version. More on this in the Installing Packs section.
 
 An optional section within the configuration file is the sideNav section:
 
-\`\`\` json
+\`\`\`json
 {
   "sideNav": [
     {
@@ -31,7 +28,7 @@ An optional section within the configuration file is the sideNav section:
 }
 \`\`\`
 
-The sideNav section allows you to specify pages that you want displayed as top level navigation items (not placed under a pack grouping).  If you installed a pack but don't want to display the default navigation items then you could use the sideNav section to surface only the pages you are interested in.
+The sideNav section allows you to specify pages that you want displayed as top level navigation items (not placed under a pack grouping). If you installed a pack but don't want to display the default navigation items then you could use the sideNav section to surface only the pages you are interested in.
 `;
 
 export default {
